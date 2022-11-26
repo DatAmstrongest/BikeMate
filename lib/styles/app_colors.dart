f1b5bad453c6116d39f6a3bef778974a251727fe
Metehan <mac@mac-MacBook-Pro.local>
2022-11-26 19:18:05 +0300

    App colors is added to the project

---
+++ lib/styles/app_colors.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const backgroundColor1 = Color.fromRGBO(209, 240, 222, 100);
+  static const backgroundColor2 = Color.fromRGBO(138, 237, 179, 100);
+  static const navbarBackgroundColor = Color.fromRGBO(2, 135, 242, 60);
+  static const popupBackgroundColor = Color.fromRGBO(148, 200, 242, 100);
+
+  static const loginTextColor = Color.fromRGBO(2, 69, 232, 100);
+  static const standardTextColor = Colors.black;
+  static const navbarSelectedTextColor = Colors.white;
+
+  static const loginButtonColor = Color.fromRGBO(2, 232, 147, 100);
+  static const signUpButtonColor = Color.fromRGBO(148, 200, 242, 100);
+  static const profilePictureButtonColor = Color.fromRGBO(38, 189, 101, 100);
+  static const followButtonColor = Color.fromRGBO(148, 200, 242, 100);
+  static const sendTextButtonColor = Color.fromRGBO(148, 200, 242, 100);
+  static const confirmButtonColor = Color.fromRGBO(148, 200, 242, 100);
+  static const deleteButtonColor = Color.fromRGBO(38, 189, 101, 100);
+}
